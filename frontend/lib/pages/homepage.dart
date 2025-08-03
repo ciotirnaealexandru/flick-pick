3aa598274da9d9281d91652e9244f98ecbd9aaae
Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>
2025-08-04 00:38:48 +0300

    Get a message from the backend
    Signed-off-by: Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>

--- frontend/lib/pages/homepage.dart
+++ frontend/lib/pages/homepage.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  String? userDashboard;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserDashboard();
+  }
+
+  Future<void> getUserDashboard() async {
+    final secureStorage = FlutterSecureStorage();
+    final token = await secureStorage.read(
+      key: dotenv.env['SECURE_STORAGE_SECRET']!,
+    );
+
+    final response = await http.get(
+      Uri.parse('${dotenv.env['API_URL']!}/auth/dashboard'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        userDashboard = response.body;
+      });
+    } else {
+      setState(() {
+        userDashboard = "No_dashboard";
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +59,8 @@ class Homepage extends StatelessWidget {
         backgroundColor: Theme.of(context).primaryColor,
         centerTitle: true,
       ),
+
+      body: Container(child: Text(userDashboard ?? "Loading..")),
     );
   }
 }
