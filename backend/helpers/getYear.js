0821af53e46ae2956d5bc8f841647e5b0e84130e
Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>
2025-08-26 23:55:31 +0300

    Make get year helper function
    Signed-off-by: Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>

--- backend/helpers/getYear.js
+++ backend/helpers/getYear.js
@@ -1,6 +1,9 @@
 function getYear(original) {
-  const match = original.match(/\d{4}/);
-  return match ? match[0] : original;
+  if (original) {
+    const match = original.match(/\d{4}/);
+    return match ? match[0] : original;
+  }
+  return null;
 }
 
 module.exports = getYear;
