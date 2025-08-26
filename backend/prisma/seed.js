96955a797c8cd925b1cd25091351beb00c0ee236
Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>
2025-08-26 19:00:51 +0300

    Implement prisma db seeding with one ADMIN and 10 USERS
    Signed-off-by: Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>

---
+++ backend/prisma/seed.js
@@ -0,0 +1,57 @@
+const { PrismaClient, Role } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
+
+const prisma = new PrismaClient();
+
+async function seedDatabase() {
+  const users = [];
+
+  console.log("Creating an ADMIN account");
+  try {
+    const res = await prisma.user.upsert({
+      where: { email: "admin@gmail.com" },
+      update: {},
+      create: {
+        email: "admin@gmail.com",
+        firstName: "Admin_firstName",
+        lastName: "Admin_lastName",
+        phone: "0000 000 000",
+        password: await bcrypt.hash("admin", 12),
+        role: Role.ADMIN,
+      },
+    });
+
+    console.log("Inserted: ", res);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+
+  console.log("Creating USER accounts");
+  try {
+    for (let i = 1; i <= 10; i++) {
+      const user = await prisma.user.upsert({
+        where: { email: `user${i}@gmail.com` },
+        update: {},
+        create: {
+          email: `user${i}@gmail.com`,
+          firstName: `User${i}_firstName`,
+          lastName: `User${i}_lastName`,
+          phone: `0000 000 000`,
+          password: await bcrypt.hash("pass", 12),
+          role: Role.USER,
+        },
+      });
+      console.log("Inserted: ", user);
+      users.push(user);
+    }
+
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+// Execute the seeder
+seedDatabase();
