02ebb4a990799c6e89124695067178b6a84bc034
Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>
2025-09-10 20:30:26 +0300

    Add an is web check for dotenv
    Signed-off-by: Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>

--- frontend/lib/services/env_service.dart
+++ frontend/lib/services/env_service.dart
@@ -3,7 +3,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class EnvConfig {
   // Load .env for dev only
   static Future<void> loadEnv() async {
-    await dotenv.load(fileName: '.env');
+    // Only load .env on mobile/desktop, not Web
+    if (!const bool.fromEnvironment('IS_WEB', defaultValue: false)) {
+      await dotenv.load(fileName: ".env.dev");
+    }
   }
 
   // Get API URL depending on environment
